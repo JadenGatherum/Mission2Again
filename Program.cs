ad6c7b53bccd0379eeb6e5f55814574b8798b332
JadenGatherum <jadenbb@byu.edu>
2024-01-17 13:17:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using Mission2Again;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int userRolls = 0;
+        int[] diceList = new int[userRolls];
+
+        System.Console.Write("Welcome to the dice throwing simulator! \nHow many dice rolls would you like to simulate? ");
+        userRolls = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS \nEach \"*\" represents 1% of the total number of rolls. \nTotal number of rolls = " + userRolls);
+
+        RollingtheDice rd = new RollingtheDice();
+
+        diceList = rd.ListofRolls(userRolls);
+
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int numCount = 0;
+            double astCount = 0;
+            int roundedAst = 0;
+            double percent = 0;
+            for (int j = 0; j < userRolls; j++)
+            {
+                if (diceList[j] == i)
+                {
+                    numCount++;
+                }
+            }
+
+            percent = (double)numCount / (double)userRolls;
+
+            astCount = percent * 100;
+
+            roundedAst = (int)Math.Round(astCount);
+
+            System.Console.Write(i + ": ");
+            for (int k = 0; k <= roundedAst; k++)
+            {
+                System.Console.Write("*");
+            }
+            System.Console.Write("\n");
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye! ");
+
+
+
+
+    }
+}
---
+++ RollingtheDice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Again
+{
+    internal class RollingtheDice
+    {
+        public int[] ListofRolls(int userRolls)
+        {
+            Random rand = new Random();
+            int[] diceList = new int[userRolls];
+
+            for (int i = 0; i < userRolls; i++)
+            {
+                int diceOne = rand.Next(1, 7);
+                int diceTwo = rand.Next(1, 7);
+                int diceCombo = diceOne + diceTwo;
+                diceList[i] = (diceCombo);
+            }
+
+            return diceList;
+        }
+    }
+}
